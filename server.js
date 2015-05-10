11501b61fd1fdb9ddb67a500b367c5cd68ce633f
Andrew Silluron <asilluron@users.noreply.github.com>
2015-05-10 16:11:21 -0700

    Setup redis. closes #26

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var Hapi = require('hapi');
 var Path = require('path');
+var redisURL = require("redis-url").parse(process.env.REDIS_URL);
+var redis = require("redis");
+var q = require("q");
 
 //Port must be an integer for HAPI
 var numPort = process.env.PORT ? parseInt(process.env.PORT, 10) : 8100;
@@ -14,9 +17,7 @@ server.views({
     partialsPath: './views/partials'
 });
 
-
 server.connection({ port: numPort });
-
 
 server.route({
     method: 'GET',
@@ -36,6 +37,38 @@ server.route({
     }
 });
 
-server.start(function () {
-    console.log("Hapi server started @ " + server.info.uri);
+var redisClient = redis.createClient(Number(redisURL.port), redisURL.hostname, {});
+
+if(redisURL.password) {
+    redisClient.auth(redisURL.password);
+}
+
+var redisDeferal = q.defer();
+var redisReady = redisDeferal.promise;
+
+redisClient.on('ready', function() {
+    redisDeferal.resolve();
 });
+
+redisClient.on('error', function(error) {
+    redisDeferal.reject(error);
+});
+
+redisReady.then(function(){
+    server.start(function () {
+        console.log("Redis client connect @ port ", redisClient.server_info.tcp_port);
+        console.log("Server started @ " + server.info.uri);
+    });
+}, function(error){
+    console.log("Could not connect to Redis server: ", error);
+});
+
+var shutdownProcess = function () {
+    redisClient.end(); //Kills all clients in their tracks
+    server.stop(function () {
+        console.log('Server stopped');
+    });
+};
+
+process.on('SIGTERM', shutdownProcess);
+process.on('SIGINT', shutdownProcess);
